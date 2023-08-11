f6070fd3fdb9cc61b50b13f0fc5ab26626a39bfc
Maria <121291247+Mashoulya@users.noreply.github.com>
2023-08-11 22:53:08 +0200

    Last changes

--- templates/script.js
+++ templates/script.js
@@ -1,21 +1,40 @@
 // GESTION DES CARTES
 
-const cardsBox = [...document.querySelectorAll('.cards-box')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
+var left = document.querySelector('.pre-btn');
+var right = document.querySelector('.nxt-btn');
+var viewport = document.querySelector('.viewport');
+var cardWidth = 240; // Исходная ширина карточек
 
-cardsBox.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
+// Функция для обновления ширины карточек в зависимости от ширины экрана
+function updateCardWidth() {
+  if (window.innerWidth <= 650) { // Примерное значение для сужения экрана
+    cardWidth = 180; // Изменяем ширину карточек для сужения экрана
+  } else {
+    cardWidth = 250; // Возвращаем исходную ширину карточек
+  }
+}
 
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
+// Обработчик для левой стрелки
+left.addEventListener('click', function() {
+   updateCardWidth(); // Обновляем ширину карточек перед прокруткой
+   viewport.scrollBy({
+      left: -cardWidth,
+      behavior: 'smooth'
+   });
+});
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+// Обработчик для правой стрелки
+right.addEventListener('click', function() {
+   updateCardWidth(); // Обновляем ширину карточек перед прокруткой
+   viewport.scrollBy({
+      left: cardWidth,
+      behavior: 'smooth'
+   });
+});
+
+// Обновляем ширину карточек при загрузке страницы и изменении размера окна
+window.addEventListener('load', updateCardWidth);
+window.addEventListener('resize', updateCardWidth);
 
 // BURGER
 
