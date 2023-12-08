c68c313d39b6e560f573cc412cf93ce6120efc9c
Maria <121291247+Mashoulya@users.noreply.github.com>
2023-12-08 18:55:35 +0100

    Gallery
    Galerie de photos sur la page d'accueil

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -53,3 +53,57 @@ const addButtons = document.querySelectorAll('.btn-add-card');
     });
 });
 
+
+// GALERIE
+
+document.addEventListener("DOMContentLoaded", function () {
+    const gallery = document.getElementById("gallery");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxContent = document.querySelector(".lightbox-content");
+
+    let currentImgIndex = 0;
+  
+    gallery.addEventListener("click", function (e) {
+      if (e.target.tagName === "IMG") {
+        const imgUrl = e.target.src;
+        currentImgIndex = Array.from(gallery.querySelectorAll('.box img')).indexOf(e.target);
+        displayImage(imgUrl);
+      }
+    });
+  
+    function displayImage(imgUrl) {
+      lightboxContent.innerHTML = `<img src="${imgUrl}" alt="">`;
+      lightbox.style.display = "block";
+
+      document.addEventListener('keydown', handleKeyPress);
+    }
+  
+    function closeLightbox() {
+      lightbox.style.display = "none";
+      document.addEventListener('keydown', handleKeyPress);
+    }
+
+    function handleKeyPress(e) {
+        if (e.keyCode === 37) {
+            navigate(-1);
+        } else if (e.keyCode === 39) {
+            navigate(1);
+        }
+    }
+    
+    function navigate(direction) {
+        currentImgIndex += direction;
+
+        const images = gallery.querySelectorAll('.box img');
+
+        if (currentImgIndex < 0) {
+            currentImgIndex = images.length - 1;
+        } else if (currentImgIndex >= images.length) {
+            currentImgIndex = 0;
+        }
+
+        const imgUrl = images[currentImgIndex].src;
+        displayImage(imgUrl);
+    }
+    window.closeLightbox = closeLightbox; // Expose la fonction pour la fermeture à l'échelle globale
+  });
